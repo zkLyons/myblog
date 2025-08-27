6b0406aa423fdcd0a8f2eddf6e66935fc4f46360
zkLyons <1649412218@qq.com>
2025-08-27 18:02:25 +0800

    commit

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,38 +4,38 @@ export default navbar([
   // 博客主页注册
   "/",
   // demo效果展示注册
-  "/demo/",
-  {
-    text: "博文",
-    icon: "pen-to-square",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "苹果",
-        icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "苹果12", icon: "pen-to-square", link: "1" },
-          "2",
-        ],
-      },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          "2",
-        ],
-      },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      "strawberry",
-    ],
-  },
+  // "/demo/",
+  // {
+  //   text: "博文",
+  //   icon: "pen-to-square",
+  //   prefix: "/posts/",
+  //   children: [
+  //     {
+  //       text: "苹果",
+  //       icon: "pen-to-square",
+  //       prefix: "apple/",
+  //       children: [
+  //         { text: "苹果12", icon: "pen-to-square", link: "1" },
+  //         "2",
+  //       ],
+  //     },
+  //     {
+  //       text: "香蕉",
+  //       icon: "pen-to-square",
+  //       prefix: "banana/",
+  //       children: [
+  //         {
+  //           text: "香蕉 1",
+  //           icon: "pen-to-square",
+  //           link: "1",
+  //         },
+  //         "2",
+  //       ],
+  //     },
+  //     { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+  //     "strawberry",
+  //   ],
+  // },
   
   {
     text: "V2 文档",
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,19 +3,19 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文章",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
+    // {
+    //   text: "如何使用",
+    //   icon: "laptop-code",
+    //   prefix: "demo/",
+    //   link: "demo/",
+    //   children: "structure",
+    // },
+    // {
+    //   text: "文章",
+    //   icon: "book",
+    //   prefix: "posts/",
+    //   children: "structure",
+    // },
     {
       text:"笔记",
       icon:"book",
@@ -23,11 +23,11 @@ export default sidebar({
       prefix: "notes/",
       children: "structure",
     },
-    "intro",
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
-    },
+    // "intro",
+    // {
+    //   text: "幻灯片",
+    //   icon: "person-chalkboard",
+    //   link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
+    // },
   ],
 });
